ffa39e48115a51f8d0f922c928f9337f847394e1
maks-developer-byte <70100557+maks-developer-byte@users.noreply.github.com>
2020-11-21 17:04:31 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+let popup =  document.getElementById('popup');
+let close_popup = document.getElementById('popup_closed');
+
+function fun1() {
+	
+	 popup.style.display = 'block';
+
+}
+function popup_closed() {
+	popup.style.display = 'none';
+}
